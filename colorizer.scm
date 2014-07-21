3649e2222e571b699560c65d22253cc5c6bf9b86
mgushee <matt@gushee.net>
2014-07-21 14:22:04 -0600

    Code loads w/o syntax errors.

--- colorizer.scm
+++ colorizer.scm
@@ -9,8 +9,8 @@
 (define (x>int x)
   (inexact->exact (round x)))
 
-;; Got this formula from http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
-;; I'm not positive it's correct. Alternative sources include:
+;; Got these formulae from http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
+;; I'm not positive they're correct. Alternative sources include:
 ;;   http://www.cs.rit.edu/~ncs/color/t_convert.html
 ;;   http://www.easyrgb.com/index.php?X=MATH&H=21#text21
 ;;   http://stackoverflow.com/questions/3018313/\
@@ -33,6 +33,27 @@
                         ((< h 300) (values x 0 c))
                         (else (values c 0 x)))))
           (values (@> (+ r* m)) (@> (+ g* m)) (@> (+ b* m))))))))
+
+(define (rgb>hsv r g b)
+  (let* ((r* (/ r 255))
+         (g* (/ g 255))
+         (b* (/ b 255))
+         (cmax (max r* g* b*))
+         (cmin (min r* g* b*))
+         (delta (- cmax cmin)))
+    (let ((h
+           (*
+             60
+             (cond
+               ((= cmax r*) (modulo (/ (- g* b*) delta) 6))
+               ((= cmax g*) (+ (/ (- b* r*) delta) 2))
+               (else (+ (/ (- r* g*) delta) 4)))))
+          (s
+            (if (= delta 0)
+              0
+              (/ delta cmax)))
+          (v cmax))
+      (values h s v))))
 
 (define (parse-color spec alpha)
   (let ((s>n (lambda (s) (string->number (string-append "#" s))))
@@ -127,9 +148,6 @@
 (define (grain-merge i m)
   (max (min (- (+ i m) 128) 255) 0))
 
-(define (blend/rgb f r0 g0 b0 r1 g1 b1)
-  (values (f r0 r1) (f g0 g1) (f b0 b1)))
-
 (define (color hi si vi hm sm vm)
   (values hm sm vi))
 
@@ -144,23 +162,7 @@
 (define (value hi si vi hm sm vm)
   (values hi si vm))
 
-(define (blend/hsv f hi si vi hm sm vm)
-  (let-values (((h s v) (f hi si vi hm sm vm)))
-    (hsv>rgb h s v)))
-
-(define (source-over r0 g0 b0 a0 r1 g1 b1 a1)
-  (let* ((a (- 1 (* (- 1 a1) (- 1 a0))))
-         (f
-           (lambda (v0 v1)
-             (/ (+ (* v1 a1) (* v0 a0 (- 1 a1))) a))))
-    (values (f r0 r1) (f g0 g1) (f b0 b1) a)))
-
-(define (composite r0 g0 b0 a0 r1 g1 b1 a1 #!optional (method 'source-over))
-  (case method
-    ((source-over) (source-over r0 g0 b0 a0 r1 g1 b1 a1))
-    (else (error (sprintf "Unsupported compositing method: '~A'" method)))))
-
-(define (calculate-color/rgb ri gi bi ai rm gm bm am mode)
+(define (blend/rgb ri gi bi rm gm bm mode)
   (let ((op (alist-ref
               mode
               `((normal . ,normal) (dissolve . ,dissolve)
@@ -172,20 +174,33 @@
                 (subtract . ,subtract) (darken-only . ,darken-only)
                 (lighten-only . ,lighten-only) (grain-extract . ,grain-extract)
                 (grain-merge . ,grain-merge)))))
-    (let-values (((rb gb bb) (blend/rgb op ri gi bi rm gm bm)))
-      (let-values (((r g b a) (composite ri gi bi ai rb gb bb am)))
-        (color/rgba r g b a)))))
+    (values (x>int (op ri rm)) (x>int (op gi gm)) (x>int (op bi bm)))))
 
-(define (calculate-color/hsv hi si vi hm sm vm mode)
-  (let ((op (alist-ref
-              mode
-              `((color . ,color)
-                (hue . ,hue)
-                (saturation . ,saturation)
-                (value . ,value)))))
-    (let-values (((rb gb bb) (blend/hsv op hi si vi hm sm vm)))
-      (let-values (((r g b a) (composite ri gi bi ai rb gb bb am)))
-        (color/rgba r g b a)))))
+(define (blend/hsv ri gi bi rm gm bm mode)
+  (let-values (((hi si vi) (rgb>hsv ri gi bi))
+               ((hm sm vm) (rgb>hsv rm gm bm)))
+    (let ((op (alist-ref
+               mode
+               `((color . ,color)
+                 (hue . ,hue)
+                 (saturation . ,saturation)
+                 (value . ,value)))))
+      (let-values (((h s v) (op hi si vi hm sm vm)))
+        (hsv>rgb h s v))))) 
+
+(define (source-over ri gi bi ai rm gm bm am)
+  (let* ((a (- 1 (* (- 1 am) (- 1 ai))))
+         (f
+           (lambda (vi vm)
+             (/ (+ (* vm am) (* vi ai (- 1 am))) a))))
+    (values (f ri rm) (f gi gm) (f bi bm) a)))
+
+(define (composite ri gi bi ai rm gm bm am #!optional (method 'source-over))
+  (let-values (((r g b a)
+                (case method
+                  ((source-over) (source-over ri gi bi ai rm gm bm am))
+                  (else (error (sprintf "Unsupported compositing method: '~A'" method))))))
+    (color/rgba r g b a)))
 
 (define (colorize src-img color-spec #!key (blend-mode 'normal) (alpha #f))
   (let* ((width (image-width src-img))
@@ -205,7 +220,7 @@
                'hsv)
              (else
                (error (sprintf "Unknown blend mode: '~A'" blend-mode))))))
-    (let-values (((r1 g1 b1 a1) (parse-color color-spec alpha)))
+    (let-values (((rm gm bm am) (parse-color color-spec alpha)))
       (let hloop ((x 0))
         (when (< x width)
           (let vloop ((y 0))
@@ -213,10 +228,10 @@
               (let-values (((ri gi bi ai) (image-pixel/rgba src-img x y)))
                 (let-values (((rb gb bb)
                               (case blend-class
-                                ((rgb) 
-                (let ((final-color
-                        (
-                  (image-draw-pixel dest final-color)))
+                                ((rgb) (blend/rgb ri gi bi rm gm bm blend-mode))
+                                ((hsv) (blend/hsv ri gi bi rm gm bm blend-mode)))))
+                (let ((final-color (composite ri gi bi ai rb rb bb am)))
+                  (image-draw-pixel dest final-color))))
               (vloop (+ y 1))))
           (hloop (+ x 1)))))
     dest))
