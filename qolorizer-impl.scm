e4316d42f8b5cea9efbb980e76aa964c87f30a3e
mgushee <matt@gushee.net>
2014-08-15 03:45:38 -0600

    Cairo image generation is working.

--- qolorizer-impl.scm
+++ qolorizer-impl.scm
@@ -1,6 +1,6 @@
 (import data-structures)
 (import extras)
-(use imlib2)
+(use cairo)
 
 (define (check1 x)
   (or (and (>= x 0) (<= x 1))
@@ -57,53 +57,6 @@
 (define (clamp255 x)
   (min (max x 0) 255))
 
-;; Got these formulae from http://www.cs.rit.edu/~ncs/color/t_convert.html
-;; I'm not positive they're correct. Alternative sources include:
-;;   http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
-;;   http://www.easyrgb.com/index.php?X=MATH&H=21#text21
-;;   http://stackoverflow.com/questions/3018313/\
-;;     algorithm-to-convert-rgb-to-hsv-and-hsv-to-rgb-in-range-0-255-for-both 
-
-;; FIXME: pretty sloppy results here!
-(define (hsv>rgb h s v)
-  (when (> h 360) (error (sprintf "Hue > 360? [~A]" h)))
-  (when (< h 0) (error (sprintf "Hue < 0? [~A]" h)))
-  (if (= s 0)
-    (values v v v)
-    (let* ((h (if (= h 360) 0 (/ h 60)))
-           (i (x>int (floor h)))
-           (f (- h i))
-           (p (* v (- 1 s)))
-           (q (* v (- 1 (* s f))))
-           (t (* v (- 1 (* s (- 1 f))))))
-      (case i
-        ((0) (values v t p))
-        ((1) (values q v p))
-        ((2) (values p v t))
-        ((3) (values p q v))
-        ((4) (values t p v))
-        (else (values v p q))))))
-
-;; ??? produces integer value for H - is that right?
-(define (rgb>hsv r g b)
-  (let* ((cmax (max r g b))
-         (cmin (min r g b))
-         (delta (- cmax cmin))
-         (v cmax))
-    (if (= delta 0)
-      (values 0 0 v)
-      (let ((h
-             (x>int
-               (*
-                 60
-                 (cond
-                   ((= cmax r) (/ (- g b) delta))
-                   ((= cmax g) (+ (/ (- b r) delta) 2))
-                   (else (+ (/ (- r g) delta) 4))))))
-            (s (/ delta cmax)))
-        (if (< h 0)
-          (values (+ h 360) s v)
-          (values h s v))))))
 
 (define (parse-color spec alpha)
   (let ((s>n
@@ -158,219 +111,32 @@
       (else
         (badspec)))))
 
-(define (normal-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) m))
-
-(define (dissolve-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (if (= (random 2) 0) i m)))
-
-(define (multiply-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (* i m)))
-
-(define (screen-op m)
-  (check1 m) 
-  (let ((a (- 1 m)))
-    (lambda (i)
-      (check1 i)
-      (- 1 (* a (- 1 i))))))
-
-(define (overlay-op m)
-  (check1 m) 
-  (let ((a (* 2 m)))
-    (lambda (i)
-      (check1 i)
-      (* i (+ i (* a (- 1 i)))))))
-
-(define (hard-light-op m)
-  (check1 m) 
-  (let ((a (* 2 (- m 0.5))))
-    (lambda (i)
-      (check1 i)
-      (if (> m 0.5)
-        (- 1 (* (- 1 a) (- 1 i)))
-        (* 2 m i)))))
-
-;; This formula seems completely broken
-; (define (soft-light i m)
-  ; (check1/2 i m)
-  ; (x>int (* (/ (+ (* (- 255 i) m) (screen i m)) 255) i)))
-
-;; Here is the W3C formula
-(define (soft-light-op m)
-  (check1 m) 
-  (let ((a (- (* 2 m) 1))
-        (b (- 1 (* 2 m))))
-    (lambda (i)
-      (check1 i)
-      (cond
-        ((and (> m 0.5) (> i 0.25))
-          (+ i (* a (- (sqrt i) i))))
-        ((> m 0.5)
-          (+ i (* a (- (* (+ (* (- (* 16 i) 12) i) 4) i) i))))
-        (else
-          (- i (* b i (- 1 i))))))))
-
-;; ???
-(define (dodge-op m)
-  (check1 m) 
-  (let ((a (- 1 m)))
-    (lambda (i) (check1 i) (if (= a 0) 1 (clamp1 (/ i a))))))
-
-;; ???
-(define (burn-op m)
-  (check1 m) 
-  (lambda (i)
-    (check1 i)
-    (if (<= m 0)
-      0
-      (- 1 (min 1 (/ (- 1 i) m))))))
-
-;; 1. Based on test results, the formula in the GIMP manual matches
-;;    program behavior, except values are constrained to 0 <= x <= 255.
-(define (divide-op m)
-  (check1 m) 
-  (lambda (i)
-    (check1 i)
-    (if (= m 0) 1 (clamp1 (/ i m)))))
-
-(define (difference-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (abs (- i m))))
-
-(define (addition-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (min (+ i m) 1)))
-
-(define (subtract-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (max (- i m) 0)))
-
-; (define darken-only min)
-
-; (define lighten-only max)
-
-;; Temporary defs for debugging
-(define (darken-only-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (min i m)))
-
-(define (lighten-only-op m)
-  (check1 m) 
-  (lambda (i) (check1 i) (max i m)))
-
-(define (grain-extract-op m)
-  (check1 m) 
-  (lambda (i)
-    (check1 i)
-    (max (min (+ (- i m) 0.5) 1) 0)))
-
-(define (grain-merge-op m)
-  (check1 m) 
-  (lambda (i)
-    (check1 i)
-    (max (min (- (+ i m) 0.5) 1) 0)))
-
-(define (color-op hm sm vm)
-  (check-hsv hm sm vm)
-  (lambda (hi si vi) (check-hsv hi si vi) (values hm sm vi)))
-  
-(define (hue-op hm sm vm)
-  (check-hsv hm sm vm)
-  (let ((hop
-         (lambda (hi si vi)
-           (if (= sm 0)
-             (values hi si vi)
-             (values hm si vi)))))
-    (lambda (hi si vi) (check-hsv hi si vi) (hop hi si vi))))
-  
-(define (saturation-op hm sm vm)
-  (check-hsv hm sm vm)
-  (lambda (hi si vi) (check-hsv hi si vi) (values hi sm vi)))
-
-(define (value-op hm sm vm)
-  (check-hsv hm sm vm)
-  (lambda (hi si vi) (check-hsv hi si vi) (values hi si vm)))
-
-
-(define (source-over ri gi bi ai rm gm bm am)
-  ; (let* ((a (- 1 (* (- 1 am) (- 1 ai)))) ; don't know where I got this
-  ; (let* ((a (- (+ ai am) (/ ai am)))     ; I think this is an error
-  (let* ((a (- (+ ai am) (* ai am)))       ; based on W3C Compositing
-         (f
-           (lambda (ci cm)
-             (/ (+ (* (- 1 ai) cm am)
-                   (* (- 1 am) ci ai)
-                   (* am ai cm))
-                a))))
-    (values (f ri rm) (f gi gm) (f bi bm) a)))
-
-(define (composite-op am #!optional (method 'source-over))
-  (let ((op
-         (case method
-           ((source-over) source-over)
-           (else (error (sprintf "Unsupported compositing method: '~A'" method))))))
-    (lambda (ri gi bi ai rm gm bm)
-      (let-values (((r g b a) (op ri gi bi ai rm gm bm am)))
-        (values (x>int255 r) (x>int255 g) (x>int255 b) (x>int255 a))))))
-
-(define (mk-blend-op color-spec #!key (blend-mode 'normal) (alpha #f))
-  (let-values (((rm gm bm am) (parse-color color-spec alpha)))
-    (let* ((mk-rgb-op
-            (alist-ref
-              blend-mode
-              `((normal . ,normal-op) (dissolve . ,dissolve-op)
-                (multiply . ,multiply-op) (screen . ,screen-op)
-                (overlay . ,overlay-op) (hard-light . ,hard-light-op)
-                (soft-light . ,soft-light-op) (dodge . ,dodge-op)
-                (burn . ,burn-op) (divide . ,divide-op)
-                (difference . ,difference-op) (addition . ,addition-op)
-                (subtract . ,subtract-op) (darken-only . ,darken-only-op)
-                (lighten-only . ,lighten-only-op) (grain-extract . ,grain-extract-op)
-                (grain-merge . ,grain-merge-op))))
-           (mk-hsv-op
-             (and (not mk-rgb-op)
-                  (or (alist-ref
-                        blend-mode
-                        `((color . ,color-op) (hue . ,hue-op)
-                          (saturation . ,saturation-op) (value . ,value-op)))
-                      (error (sprintf "Invalid blend mode: '~A'" blend-mode)))))
-           (composite
-             (composite-op am))
-           (blend
-             (if mk-rgb-op
-               (let ((rop (mk-rgb-op rm))
-                     (gop (mk-rgb-op gm))
-                     (bop (mk-rgb-op bm)))
-                 (lambda (ri gi bi) (values (rop ri) (gop gi) (bop bi))))
-               (let-values (((hm sm vm) (rgb>hsv rm gm bm)))
-                 (let ((base-op (mk-hsv-op hm sm vm)))
-                   (lambda (ri gi bi)
-                     (let-values (((hi si vi) (rgb>hsv ri gi bi)))
-                       (let-values (((h s v) (base-op hi si vi)))
-                         (hsv>rgb h s v)))))))))
-      (lambda (ri gi bi ai)
-        (let-values (((rb gb bb) (blend ri gi bi)))
-          (composite ri gi bi ai rb gb bb))))))
+(define (get-cairo-blend-mode mode)
+  (alist-ref
+    mode
+    `((multiply . ,CAIRO_OPERATOR_MULTIPLY) (screen . ,CAIRO_OPERATOR_SCREEN)
+      (overlay . ,CAIRO_OPERATOR_OVERLAY) (darken-only . ,CAIRO_OPERATOR_DARKEN)
+      (lighten-only . ,CAIRO_OPERATOR_LIGHTEN) (dodge . ,CAIRO_OPERATOR_COLOR_DODGE)
+      (burn . ,CAIRO_OPERATOR_COLOR_BURN) (hard-light . ,CAIRO_OPERATOR_HARD_LIGHT)
+      (soft-light . ,CAIRO_OPERATOR_SOFT_LIGHT) (difference . ,CAIRO_OPERATOR_DIFFERENCE)
+      (exclusion . ,CAIRO_OPERATOR_EXCLUSION) (hue . ,CAIRO_OPERATOR_HSL_HUE)
+      (saturation . ,CAIRO_OPERATOR_HSL_SATURATION) (color . ,CAIRO_OPERATOR_HSL_COLOR)
+      (luminosity . ,CAIRO_OPERATOR_HSL_LUMINOSITY))))
                     
+(define (mk-blend-op color-spec #!key (blend-mode 'normal) (alpha #f))
+  (let-values (((r g b a) (parse-color color-spec alpha)))
+    (let ((mode-spec (get-cairo-blend-mode blend-mode)))
+      (lambda (src-file dest-file)
+        (let* ((surf (cairo-image-surface-create-from-png src-file))
+               (width (cairo-image-surface-get-width surf)) 
+               (height (cairo-image-surface-get-height surf)) 
+               (ctx (cairo-create surf)))
+          (cairo-set-source-rgba ctx r g b a)
+          (cairo-set-operator ctx mode-spec)
+          (cairo-rectangle ctx 0 0 width height)
+          (cairo-fill ctx)
+          (cairo-surface-write-to-png surf dest-file)
+          (cairo-surface-destroy surf))))))
 
-(define (colorize src-img pixel-op)
-  (let* ((width (image-width src-img))
-         (height (image-height src-img))
-         ; (dest (image-create width height)))
-         (dest (image-clone src-img)))
-    (let hloop ((x 0))
-      (when (< x width)
-        (let vloop ((y 0))
-          (when (< y height)
-            (let-values (((ri* gi* bi* ai*) (image-pixel/rgba src-img x y)))
-              (let-values (((ri gi bi ai) (values (/ ri* 255) (/ gi* 255) (/ bi* 255) (/ ai* 255))))
-                ; (let ((final-color (pixel-op ri gi bi ai)))
-                (let-values (((r g b a) (pixel-op ri gi bi ai)))
-                  (image-draw-pixel dest (color/rgba r g b a) x y))))
-          (vloop (+ y 1))))
-        (hloop (+ x 1))))
-    dest))
-
+(define (colorize blend-op src-file dest-file)
+  (blend-op src-file dest-file))
