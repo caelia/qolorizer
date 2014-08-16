62b37e749a8f0a9770286028b0aeab2657e74fa0
mgushee <matt@gushee.net>
2014-08-15 18:32:58 -0600

    Began command line application.

---
+++ qolorizer-cl.scm
@@ -0,0 +1,11 @@
+(use qolorizer)
+(use getopt-long)
+
+(define (mk-dest-name pattern filename)
+  #f)
+  
+(define (process-file src color-spec #!key (blend-mode 'normal) (alpha 1.0) (dest #f) (dest-pattern #f))
+  #f)
+  
+(define (process-dir src color-spec #!key (blend-mode 'normal) (alpha 1.0) (dest #f) (dest-pattern #f))
+  #f)
