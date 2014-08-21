dab78d589462f2e77f45054c8b176773b8f2030c
mgushee <matt@gushee.net>
2014-08-20 18:45:18 -0600

    Needed to commit before merging.

--- qolorizer-cl.scm
+++ qolorizer-cl.scm
@@ -1,7 +1,9 @@
 (use qolorizer)
+(use irregex)
 (use getopt-long)
 
 (define (mk-dest-name pattern filename)
+    
   #f)
   
 (define (process-file src color-spec #!key (blend-mode 'normal) (alpha 1.0) (dest #f) (dest-pattern #f))
